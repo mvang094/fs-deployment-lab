6b5a6f9eb4bf75bd5dc17708534ef230c77ea167
Mvang094 <mvang094@gmail.com>
2022-08-31 13:21:46 -0500

    Changed app get again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 })
 
 const port = 4006;
